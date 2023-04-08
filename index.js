3a09bf7e4e57f38a6df250c5eca2eb30de62953c
GevorgMartirosyan8878 <gevorg.martirosyan.1998.08@gmail.com>
2023-04-09 01:45:14 +0400

    added object and map operations

--- index.js
+++ index.js
@@ -4,3 +4,26 @@ function countDuration(callback, ...args) {
   const end = Date.now();
   return end - start;
 }
+
+const object = {};
+const map = new Map();
+
+function makeObjectOperation(start = 0, end) {
+  for (let i = start; i < end; i++) {
+    object[i] = i;
+    delete object[i];
+  }
+}
+
+function makeMapOperation(start = 0, end) {
+  for (let i = start; i < end; i++) {
+    map.set(i, i);
+    map.delete(i);
+  }
+}
+
+const objectOperationDuration = countDuration(makeObjectOperation, 0, 1000000);
+const mapOperationDuration = countDuration(makeMapOperation,0, 1000000);
+
+console.log(`Object operation duration: ${objectOperationDuration}ms`);
+console.log(`Map operation duration: ${mapOperationDuration}ms`);
